743b4f4399cb76a031249f91f0339d495cd15b76
RushikeshHodade <72199451+RushikeshHodade@users.noreply.github.com>
2020-10-01 16:33:44 +0530

    Update emailphonescraping.py

--- emailphonescraping.py
+++ emailphonescraping.py
@@ -20,25 +20,25 @@ phone = re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})",s)
 emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",s)
 
 if len(phone) == 0:
-    print ("Sorry, no phone number found.")
+    print ("Sorry, No phone number found.")
 
     print('------------')
     print ()
 else :
     count = 1
     for item in phone:
-        print ( count, ' phone number(s) found : ',item )
+        print ( count, ' Phone number(s) found : ',item )
         count += 1
 
 print('------------')
 print()
 
 if len(emails) == 0:
-    print("Sorry, no email address found.")
+    print("Sorry, No email address found.")
     print('------------')
     print()
 else:
     count = 1
     for item in emails:
-        print(count, ' email address(es) found : ', item)
+        print(count, ' Email address(es) found : ', item)
         count += 1
